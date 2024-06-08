f983c141d864313aabdb47a5c2fa9861a70d52bf
basitiqbal <40729868+basitiqbal@users.noreply.github.com>
2024-06-08 19:24:29 +0500

    Add files via upload

---
+++ 20_Storing_Array.ts
@@ -0,0 +1,6 @@
+// Store names of famous Person in an array
+let famous_Person: string[] = ["Quaid-e-Azam", "Allama Iqbal", "Liqauat Ali Khan", "Rashid Minhas", "Fatima Jinnah"];
+
+// Print the array containing the names of rivers
+console.log("List of famous Person: ");
+console.log(famous_Person);
