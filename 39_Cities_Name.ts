c1776c63d1183aa18a1303595bd3b715fb12e0b8
basitiqbal <40729868+basitiqbal@users.noreply.github.com>
2024-06-08 21:00:43 +0500

    Add files via upload

---
+++ 39_Cities_Name.ts
@@ -0,0 +1,8 @@
+function city_country(city: string, country: string) {
+    return `${city}, ${country}`;
+}
+
+// Calling the function for three different cities
+console.log(city_country("Karachi", "Pakistan"));
+console.log(city_country("Jaddah", "Saudia Arab"));
+console.log(city_country("Emirate's ", "Dubai"));
