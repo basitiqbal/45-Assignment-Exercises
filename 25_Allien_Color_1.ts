f1242a2698b522cae22e5f8b5c96421533e9d699
basitiqbal <40729868+basitiqbal@users.noreply.github.com>
2024-06-08 20:50:02 +0500

    Add files via upload

---
+++ 25_Allien_Color_1.ts
@@ -0,0 +1,7 @@
+let alien_color: string = 'yellow';
+
+if (alien_color == 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+} else {
+    console.log("You didn't shoot down a green alien. No points earned.");
+}
