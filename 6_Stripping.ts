16d7caf812f8ad3472c6d33d5a085227a29f7bf0
basitiqbal <40729868+basitiqbal@users.noreply.github.com>
2024-06-08 19:17:22 +0500

    Add files via upload

---
+++ 6_Stripping.ts
@@ -0,0 +1,9 @@
+
+let personNameWithWhitespace: string = "\n\t Basit Iqbal\t\n";
+
+
+console.log(personNameWithWhitespace);
+
+let strippedName: string = personNameWithWhitespace.trim();
+
+console.log("Stripped name:", strippedName);
