0332d3fafba97a1046eb3bdaf7c9b1a0fc277248
basitiqbal <40729868+basitiqbal@users.noreply.github.com>
2024-06-08 19:23:33 +0500

    Add files via upload

---
+++ 19_Dinner.ts
@@ -0,0 +1,6 @@
+let myFriends=["Moiz","Farooq","Danish","Abdullah"];
+console.log(`I m inviting ${myFriends.length} no of people in my dinner invition.`);
+for(let i=0; i<myFriends.length; i++)
+{
+    console.log(`${i+ 1}.${myFriends[i]}`);
+}
