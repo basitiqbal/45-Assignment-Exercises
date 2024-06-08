4ccefb5f6f51cde12c15152e1045033b8af28929
basitiqbal <40729868+basitiqbal@users.noreply.github.com>
2024-06-08 20:52:17 +0500

    Add files via upload

---
+++ 27_Allien_Color_3.ts
@@ -0,0 +1,36 @@
+//Version for a green alien:
+
+let alien_color: string = 'green';
+
+if (alien_color == 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+} else if (alien_color == 'yellow') {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+} else if (alien_color == 'red') {
+    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+}
+//Version for a yellow alien:
+
+
+alien_color = 'yellow';
+
+if (alien_color == 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+} else if (alien_color == 'yellow') {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+} else if (alien_color == 'red') {
+    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+}
+
+//Version for a red alien:
+
+alien_color  = 'red';
+
+if (alien_color == 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+} else if (alien_color == 'yellow') {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+} else if (alien_color == 'red') {
+    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+}
+
