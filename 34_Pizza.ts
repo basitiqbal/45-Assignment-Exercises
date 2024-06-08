fc01acc818f068ee9b0d6118e1ba5f0a184f0d3e
basitiqbal <40729868+basitiqbal@users.noreply.github.com>
2024-06-08 20:57:21 +0500

    Add files via upload

---
+++ 34_Pizza.ts
@@ -0,0 +1,7 @@
+let favorite_pizzas: string[] = ["Pepperoni", "Fajita", "Supreme Chicken"];
+
+for (let pizza of favorite_pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+
+console.log("Pizza is one of my favorite foods. I really love pizza!");
