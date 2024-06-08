31a6177e70598b5da84434f6d69228ca140f00dc
basitiqbal <40729868+basitiqbal@users.noreply.github.com>
2024-06-08 20:43:05 +0500

    Add files via upload

---
+++ 21_Objects.ts
@@ -0,0 +1,25 @@
+// Define TypeScript objects containing information about Personalities
+let person_1 = {
+    name: "Basit",
+    age: 27,
+    cast: "Punjabi"
+};
+
+let person_2 = {
+    name: "Moiz",
+    age: 25,
+    cast: "Mahajir"
+};
+
+let person_3 = {
+    name: "Farooq",
+    age: 30,
+    cast: "Qureshi"
+};
+
+
+
+// Print the information about each book
+console.log("Person 1:", person_1);
+console.log("Person 2:", person_2);
+console.log("Person 3:", person_3);
