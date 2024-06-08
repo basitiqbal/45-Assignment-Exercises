51f4695967bc672fa4ec329a456f67bfb43ca749
basitiqbal <40729868+basitiqbal@users.noreply.github.com>
2024-06-08 20:55:16 +0500

    Add files via upload

---
+++ 31_No_User.ts
@@ -0,0 +1,9 @@
+let usernames: string[] = ['basit','moiz','farooq','admin']; // Empty array of usernames
+
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+} else {
+    usernames=[];
+    console.log("remove name from user. "+ usernames.length);
+
+}
