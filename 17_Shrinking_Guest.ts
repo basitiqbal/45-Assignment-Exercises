3e77609a56f6cdc521f4d1c462c53ea7aecc6262
basitiqbal <40729868+basitiqbal@users.noreply.github.com>
2024-06-08 19:22:18 +0500

    Add files via upload

---
+++ 17_Shrinking_Guest.ts
@@ -0,0 +1,33 @@
+//making guest list array
+let guestList: string[] = ["Moiz", "Farooq", "Danish","Sarim","Muhib","Jujjan"];
+
+let dontCame = guestList[0];// not came equal to 0 index
+console.log(dontCame, `Not Came;~`);
+
+guestList.splice(0, 1, "Abdullah");// add or remove from guest list array
+console.log("\nGreat news! I found a bigger dinner table!");
+guestList.unshift("Jamal");// add new  begining of guest list 
+// add a new value ending array
+guestList.push("Hamid");
+//adding middle index of array
+
+let middleIndex: number = Math.floor(guestList.length / 2);
+//adding new guest in the middle of array
+guestList.splice(middleIndex, 1, "Haris");
+console.log("\n\t -:Updated List of Guest are Here:-\n");
+guestList.forEach(oneguest => console.log(`Dear ${oneguest},  would like to invite you to dinner. It would be an honor to have your company.`));
+
+// Simulate not enough space for all guests
+console.log("\nUnfortunately, we can only invite two people for dinner.");
+//using while loop to remove guest from the array until only 2 names remain
+while (guestList.length>2) {
+let removeGuest=guestList.pop();
+console.log(`SORRY ${removeGuest}, You are not invvited.`)
+}
+console.log(`InvitatioN to the last two guests`);
+guestList.forEach(lastTwo=> console.log(`Great! ${lastTwo} You are invited to dinner.`));
+//Removing last two guest from the list
+guestList.pop();
+guestList.pop();
+
+console.log(`Empty List : `,guestList);
