a32d116c2e968719e97e2700577a481710394fbb
basitiqbal <40729868+basitiqbal@users.noreply.github.com>
2024-06-08 19:18:21 +0500

    Add files via upload

---
+++ 10_(1)_Comments.ts
@@ -0,0 +1,5 @@
+// Program # 10 "Print Hello World in Console File"
+
+// Written By: Basit
+//Dated: 2-3-2024
+console.log("Hello World!");
---
+++ 10_(2).ts
@@ -0,0 +1,13 @@
+// Program # 10 b "adding 2 number and print in a new variable "
+
+// Written By: Basit
+//Dated: 2-3-2024
+// store variable and result 
+
+
+
+const num1: number = 5;
+const num2: number = 10;
+const result=num1+num2;
+
+console.log("The sum is :", result);
