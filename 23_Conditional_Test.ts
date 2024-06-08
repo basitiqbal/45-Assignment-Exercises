0fcb09c768126e0055f8befa6cce6f70ce1d41e9
basitiqbal <40729868+basitiqbal@users.noreply.github.com>
2024-06-08 20:44:50 +0500

    Add files via upload

---
+++ 23_Conditional_Test.ts
@@ -0,0 +1,31 @@
+let car = 'subaru';
+//test 1
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car=='subaru');
+//test 2
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car==='subaru');
+//test 3
+console.log("Is car !='Toyota'? I predict True.");
+console.log(car!='Toyota');
+//test 4
+console.log("Is car !='BIKE'? I predict True.");
+console.log(car!='BIKE');
+//test 5
+console.log("Is car IS UPPERCASE  IS EQUAL TO 'SUBARU'? I predict True.");
+console.log(car.toUpperCase() =='SUBARU');
+//test 6
+console.log("Is car is in lower case  'subaru'? I predict false.");
+console.log(car.toLowerCase() =='sSubaru');
+//test 7
+console.log("Is car IS EQUAL TO 'subaru'? I predict False.");
+console.log(car =='SUBARU');
+//test 8
+console.log("Is car === 'SUBARU'? I predict False.");
+console.log(car === 'SUBARU');
+//test 9
+console.log("Is car === 'Bus'? I predict False.");
+console.log(car === 'bus');
+//test 10
+console.log("Is car === 'civic'? I predict False.");
+console.log(car === 'civic');
